9d9ec65e43f137b125493d461af6572af24c619a
raphael laranjeira <raphagobatto@hotmail.com>
2023-01-14 15:51:15 -0300

    first commit

---
+++ db/migrate/20230114184958_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :genre
+      t.integer :year
+      t.string :country
+      t.date :published_at
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
