e7a8efe529b6a7fb3f7ff571333e051df8153a1a
raphael laranjeira <raphagobatto@hotmail.com>
2023-01-16 16:57:40 -0300

    api-controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::APIController < ActionController::API
+class Api::V1::ApiController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_14_193824) do
+ActiveRecord::Schema.define(version: 2023_01_15_140145) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2023_01_14_193824) do
     t.string "country"
     t.date "published_at"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "titles", force: :cascade do |t|
+    t.string "genre"
+    t.string "year"
+    t.string "country"
+    t.string "published_at"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
