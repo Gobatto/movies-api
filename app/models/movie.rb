c2ff427b6b463ff4ba723bd07a9baff01f702d84
raphael laranjeira <raphagobatto@hotmail.com>
2023-01-16 10:09:48 -0300

    creating api

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,11 @@
 class Movie < ApplicationRecord
   validates :id, uniqueness: true
   validates :title, presence: true, uniqueness: true
+    # validates that there are no duplicates
 
   include PgSearch::Model
 
   pg_search_scope :search, against: %i[title genre year country]
+  # allow a user to enter a query and search across a couple different attributes on models.
+  
 end
