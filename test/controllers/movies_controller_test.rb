a07f9b49e9882e43cc348064b3f48b0587fe38d4
raphael laranjeira <raphagobatto@hotmail.com>
2023-01-17 10:36:26 -0300

    tests

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -4,4 +4,33 @@ class Api::V1::MoviesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get index" do
+    get api_v1_movies_url
+    assert_response :success
+  end
+
+  test "should get movie title" do
+    get api_v1_movies_url, params: { title: "Batman: The Killing Joke" }
+    assert_response :success
+  end
+
+  test "should get movie genre" do
+    get api_v1_movies_url, params: { genre: "Movie" }
+    assert_response :success
+  end
+
+  test "should get movie year" do
+    get api_v1_movies_url, params: { year: "2020" }
+    assert_response :success
+  end
+
+  test "should get movie country" do
+    get api_v1_movies_url, params: { country: "United States"}
+    assert_response :success
+  end
+
 end
+
+
+# s765,Movie,Batman: The Killing Joke,Sam Liu,"Kevin Conroy, Mark Hamill, Tara Strong, Ray Wise, John DiMaggio, Robin Atkin Downes, Brian George, JP Karliak, Andrew Kishino, Nolan North",United States,"October 15, 2020",2016,R,77 min,Action & Adventure,The Joker makes life hell for the Gordon family in a monstrous attempt to prove to Batman that one bad day can drive a good man to madness.
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -4,4 +4,24 @@ class MovieTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'valid movie' do
+    movie = Movie.new(title: 'Batman: The Killing Joke',
+                      genre: 'Movie',
+                      year: '2020',
+                      country: 'United States',
+                      published_at: '2020-10-15',
+                      description: "The Joker makes life hell for the Gordon family in a monstrous attempt to prove to Batman that
+                                    one bad day can drive a good man to madness.")
+    assert movie.valid?
+  end
+
+  test 'invalid movie' do
+    movie = Movie.new(genre: 'TV Show',
+                      year: '2017',
+                      country: 'Canada',
+                      published_at: '2018-03-01',
+                      description: "Players and coaches for a Montreal soccer team chase dreams of stardom while their personal lives
+                                    erupt with love, fear, passion and violence.")
+    assert_not movie.valid?
+  end
 end
